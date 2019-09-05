7f7a893199342899c9962bbf82469163ca1795d0
Esteban Frare <esteban.frare@radiumrocket.com>
2019-09-05 18:09:57 -0300

    VC: add new splash screen on Android

--- android/app/src/main/java/com/vcardgenerator/MainActivity.java
+++ android/app/src/main/java/com/vcardgenerator/MainActivity.java
@@ -10,12 +10,9 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
-        super.onCreate(savedInstanceState);
-    }
+    
     protected String getMainComponentName() {
+        SplashScreen.show(this);  // here
         return "vCardGenerator";
     }
 }
